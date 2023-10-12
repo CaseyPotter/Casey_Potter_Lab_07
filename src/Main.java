05d9af95c87478cd28a06105c87804042a9b30db
CaseyPotter <potterc3@mail.uc.edu>
2023-10-12 12:51:34 -0400

    init

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+public class Main
+{
+    public static void main(String[] args) {
+//task A1
+        System.out.println("Increasing");
+        for(int r=0; r < 31; r++)
+        {
+            System.out.println(""  + r );
+        }
+//task A2
+        System.out.println("Decreasing");
+        for(int r=30; r > 0; r--)
+        {
+            System.out.println(""  + r );
+        }
+    }
+}
